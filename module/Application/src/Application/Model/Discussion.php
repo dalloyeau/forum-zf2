e1084b7691ec6a1646b0e40836ddd9d0bfcb87d3
dalloyeau <david.dalloyeau@gmail.com>
2013-12-21 02:21:52 +0100

    suite ajout discussion

--- module/Application/src/Application/Model/Discussion.php
+++ module/Application/src/Application/Model/Discussion.php
@@ -1,5 +1,11 @@
 <?php
 namespace Application\Model;
+use Zend\Db\ResultSet\Row;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\Factory as InputFactory;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
+
 
 class Discussion
 {
@@ -13,4 +19,74 @@ class Discussion
 		$this->idTheme = (!empty($data['idTheme'])) ? $data['idTheme'] : null;
 		$this->titre  = (!empty($data['titre'])) ? $data['titre'] : null;
 	}
+}
+
+
+class Album extends Row implements InputFilterAwareInterface
+{
+    protected $inputFilter;
+    
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new \Exception("Not used");
+    }
+    
+    public function getInputFilter()
+    {
+        if (!$this->inputFilter) {
+            $inputFilter = new InputFilter();
+            $factory = new InputFactory();
+            
+            $inputFilter->add($factory->createInput(array(
+                'name' => 'id',
+                'required' => true,
+                'filters' => array(
+                    array('name' => 'Int'),
+                ),
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name' => 'artist',
+                'required' => true,
+                'filters' => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name' => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min' => 1,
+                            'max' => 100,
+                        ),
+                    ),
+                ),
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name' => 'title',
+                'required' => true,
+                'filters' => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name' => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min' => 1,
+                            'max' => 100,
+                        ),
+                    ),
+                ),
+            )));
+            
+            $this->inputFilter = $inputFilter;
+        }
+        
+        return $this->inputFilter;
+    }
+    
 }
